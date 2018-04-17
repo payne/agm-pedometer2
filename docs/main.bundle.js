1088a6803c60c77340c301d85b5e9c38eba15ba7
Matt Payne <Payne@MattPayne.org>
2018-04-16 19:58:46 -0500

    ng build; cp dist/* docs

--- docs/main.bundle.js
+++ docs/main.bundle.js
@@ -27,7 +27,7 @@ module.exports = "agm-map { height: 500px; }\n\n"
 /***/ "./src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<h1>Slowly learning <a href=\"https://angular-maps.com/\">AGM</a> One step at a time...</h1>\n\n<h2>Distance: {{distance}} miles</h2>\n\n<agm-map [latitude]=\"lat\" [longitude]=\"lng\" (mapClick)=\"mClick($event)\"\t\n\t\t\t\t[usePanning]='true' [mapTypeId]=\"mapTypeId\">\n\n\t  <agm-marker [latitude]=\"lat\" [longitude]=\"lng\"></agm-marker>\n\t\t\t\t\t\t\t\n\t\t<agm-kml-layer [url]=\"kmlurl\"></agm-kml-layer>\n\n\t\t<agm-polyline>\n\t\t <!-- the mClick method adds to the mapPointer array.  Sweet! -->\n     <agm-polyline-point *ngFor=\"let pointer of mapPointer\"\n      [latitude]=\"pointer.latitude\"\n      [longitude]=\"pointer.longitude\">\n     </agm-polyline-point>\n  </agm-polyline>\n\n</agm-map>\n\n  <div class=\"github-banner\">\n    <a href=\"https://github.com/payne/agm-pedometer2\" class=\"github-corner\" aria-label=\"View source on Github\">\n      <svg width=\"60\" height=\"60\" viewBox=\"0 0 250 250\" style=\"z-index: 11; fill:#FD6C6C; color:#fff; position: absolute; top: 0; border: 0; right: 0;\" aria-hidden=\"true\"><path d=\"M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z\"></path><path d=\"M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2\" fill=\"currentColor\" style=\"transform-origin: 130px 106px;\" class=\"octo-arm\"></path><path d=\"M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z\" fill=\"currentColor\" class=\"octo-body\"></path>\n      </svg>\n    </a>\n    <style>\n      .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}\n    </style>\n  </div>\n\n"
+module.exports = "<h1>Slowly learning <a href=\"https://angular-maps.com/\">AGM</a> One step at a time...</h1>\n\n<h2>Distance: {{distance}} miles</h2>\n\n<agm-map [latitude]=\"lat\" [longitude]=\"lng\" (mapClick)=\"mClick($event)\"\t\n\t\t\t\t[usePanning]='true' [mapTypeId]=\"mapTypeId\">\n\n\t  <agm-marker [latitude]=\"lat\" [longitude]=\"lng\"></agm-marker>\n\t\t\t\t\t\t\t\n\t\t<agm-kml-layer [url]=\"kmlurl\"></agm-kml-layer>\n\n\t\t<agm-polyline>\n\t\t <!-- the mClick method adds to the mapPointer array.  Sweet! -->\n     <agm-polyline-point *ngFor=\"let pointer of mapPointer\"\n      [latitude]=\"pointer.latitude\"\n      [longitude]=\"pointer.longitude\">\n     </agm-polyline-point>\n  </agm-polyline>\n\n</agm-map>\n\n<div>\n  <ngx-datatable [rows]=\"rows\" [columns]=\"columns\"> </ngx-datatable>\n</div>\n\n  <div class=\"github-banner\">\n    <a href=\"https://github.com/payne/agm-pedometer2\" class=\"github-corner\" aria-label=\"View source on Github\">\n      <svg width=\"60\" height=\"60\" viewBox=\"0 0 250 250\" style=\"z-index: 11; fill:#FD6C6C; color:#fff; position: absolute; top: 0; border: 0; right: 0;\" aria-hidden=\"true\"><path d=\"M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z\"></path><path d=\"M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2\" fill=\"currentColor\" style=\"transform-origin: 130px 106px;\" class=\"octo-arm\"></path><path d=\"M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z\" fill=\"currentColor\" class=\"octo-body\"></path>\n      </svg>\n    </a>\n    <style>\n      .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}\n    </style>\n  </div>\n\n"
 
 /***/ }),
 
@@ -60,6 +60,16 @@ var AppComponent = /** @class */ (function () {
         this.mapTypeId = 'terrain';
         this.kmlurl = 'http://MattPayne.org/agm-pedometer2/Friday.kml';
         this.mapPointer = [];
+        this.rows = [
+            { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+            { name: 'Dany', gender: 'Male', company: 'KFC' },
+            { name: 'Molly', gender: 'Female', company: 'Burger King' },
+        ];
+        this.columns = [
+            { prop: 'name' },
+            { name: 'Gender' },
+            { name: 'Company' }
+        ];
     }
     AppComponent.prototype.mClick = function (e) {
         this.mapPointer.push({ latitude: e.coords.lat, longitude: e.coords.lng });
@@ -92,11 +102,13 @@ var AppComponent = /** @class */ (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__agm_core__ = __webpack_require__("./node_modules/@agm/core/index.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__("./src/app/app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__agm_js_marker_clusterer__ = __webpack_require__("./node_modules/@agm/js-marker-clusterer/index.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_ng2_haversine__ = __webpack_require__("./node_modules/ng2-haversine/index.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_ng2_haversine___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_ng2_haversine__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__swimlane_ngx_datatable__ = __webpack_require__("./node_modules/@swimlane/ngx-datatable/release/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__swimlane_ngx_datatable___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__swimlane_ngx_datatable__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__agm_core__ = __webpack_require__("./node_modules/@agm/core/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("./src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__agm_js_marker_clusterer__ = __webpack_require__("./node_modules/@agm/js-marker-clusterer/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_ng2_haversine__ = __webpack_require__("./node_modules/ng2-haversine/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_ng2_haversine___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_ng2_haversine__);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -110,23 +122,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["NgModule"])({
             declarations: [
-                __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]
             ],
             imports: [
-                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
-                __WEBPACK_IMPORTED_MODULE_2__agm_core__["a" /* AgmCoreModule */].forRoot({
+                __WEBPACK_IMPORTED_MODULE_2__swimlane_ngx_datatable__["NgxDatatableModule"],
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["BrowserModule"],
+                __WEBPACK_IMPORTED_MODULE_3__agm_core__["a" /* AgmCoreModule */].forRoot({
                     apiKey: 'AIzaSyAqxjtsvw8b-EVZ1sPk1ZJ8dh8jKl7eDU0'
                 }),
-                __WEBPACK_IMPORTED_MODULE_4__agm_js_marker_clusterer__["a" /* AgmJsMarkerClustererModule */]
+                __WEBPACK_IMPORTED_MODULE_5__agm_js_marker_clusterer__["a" /* AgmJsMarkerClustererModule */]
             ],
-            providers: [__WEBPACK_IMPORTED_MODULE_2__agm_core__["b" /* GoogleMapsAPIWrapper */], __WEBPACK_IMPORTED_MODULE_5_ng2_haversine__["HaversineService"]],
-            bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
+            providers: [__WEBPACK_IMPORTED_MODULE_3__agm_core__["b" /* GoogleMapsAPIWrapper */], __WEBPACK_IMPORTED_MODULE_6_ng2_haversine__["HaversineService"]],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
         })
     ], AppModule);
     return AppModule;
